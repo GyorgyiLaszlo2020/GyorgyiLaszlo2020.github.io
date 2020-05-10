20515d3a6ab293372271f084547ca6ad42f94066
GyorgyiLaszlo2020 <gyorgyi238923@gmail.com>
2020-05-10 17:08:07 +0200

    memory
    memory

--- scripts.js
+++ scripts.js
@@ -1,126 +1,59 @@
-
-let array = [
-    {
-        picture: "url('pictures/1Lena.jpg')",
-        title: "Léna",
-        hint: "Chateau de Chambord",
-        descr: "Léna méricskéli a fotós bácsit"
-    }, 
-    /*{
-        picture: "url('pictures/4Abi.jpg')",
-        title: "Abigél",
-        hint: "Tower Bridge",
-        descr: "Abi téglát pakol"
-    },*/
-    {
-        picture: "url('pictures/2Lena.jpg')",
-        title: "Léna",
-        hint: "Goðafoss",
-        descr: "Léna spárgázik, mint Van Damme"
+$(document).ready(function() {
+  var app = {
+    cards: ["Dalma", "Dalma", "Blanka", "Blanka", "Alíz", "Alíz", "Emma", "Emma", "Sebi", "Sebi", "Hanna", "Hanna"],
+    init: function() {
+      app.shuffle();
     },
-    /*{
-        picture: "url('pictures/3SzaboEvi.jpg')",
-        title: "Évici & Szabó",
-        hint: "Évici és Szabó",
-        descr: "Valamelyik karácsony"
+    shuffle: function() {
+      var random = 0;
+      var temp = 0;
+      for (i = 1; i < app.cards.length; i++) {
+        random = Math.round(Math.random() * i);
+        temp = app.cards[i];
+        app.cards[i] = app.cards[random];
+        app.cards[random] = temp;
+      }
+      app.assignCards();
+      console.log('Shuffled Card Array: ' + app.cards);
     },
-    {
-        picture: "url('pictures/5GyorgyiDavid.jpg')",
-        title: "Györgyi & Dávid",
-        hint: "cable car",
-        descr: "Talán Zsolti esküvőjén- Pápa"
+    assignCards: function() {
+      $('.card').each(function(index) {
+        $(this).attr('data-card-value', app.cards[index]);
+      });
+      app.clickHandlers();
     },
-    {
-        picture: "url('pictures/7RekaBalazs.jpg')",
-        title: "Réka & Balázs",
-        hint: "Stockholm",
-        descr: "Györgyi és Dávid esküvőjén 2011.01.15-én :)"
+    clickHandlers: function() {
+      $('.card').on('click', function() {
+        $(this).html('<p>' + $(this).data('cardValue') + '</p>').addClass('selected');
+        app.checkMatch();
+      });
     },
-    {
-        picture: "url('pictures/6Mama.jpg')",
-        title: "Mama",
-        hint: "Stonehenge",
-        descr: "Pápán Mama Zsoltival és Petrával."
-    },*/
-];
-
-let index = 0;
-let hintTop = -45;
-let HintLeft = 455;
-
-function refresh (index) {
-    $(".pictures").css ("background-image", array[index].picture);
-    $(".pictures>.text>h1").text (array[index].title); 
-    $(".pictures>.text>p").text (array[index].descr);
-
-}
-
-function imgHandler (event) {
-    console.log ("ImgHandler meghivodtam");
-
-    index = $(this).data('big');
-    $(this).parent().parent().find('.thumbnail').removeClass('activethumbnail');
-    $(this).addClass('activethumbnail');
-    refresh (index);
-}
-
-function leftHandler (event) {
-    console.log ("leftHandler meghivodtam");
-
-    if (index === 0) {
-            index = array.length - 1;
-            $(".thumbnails").children().last().children().first().trigger("click");
-    } else {
-        index--;
-        $(".activethumbnail").parent().prev().children().first().trigger("click");
+    checkMatch: function() {
+      if ($('.selected').length === 2) {
+        if ($('.selected').first().data('cardValue') == $('.selected').last().data('cardValue')) {
+          $('.selected').each(function() {
+            $(this).animate({
+              opacity: 0
+            }).removeClass('unmatched');
+          });
+          $('.selected').each(function() {
+            $(this).removeClass('selected');
+          });
+          app.checkWin();
+        } else {
+          setTimeout(function() {
+            $('.selected').each(function() {
+              $(this).html('').removeClass('selected');
+            });
+          }, 1000);
+        }
+      }
+    },
+    checkWin: function() {
+      if ($('.unmatched').length === 0) {
+        $('.container').html('<h1>Gratulálok, nyertél!</h1>');
+      }
     }
-    
-    //refresh (index);
-}
-
-function rightHandler (event) {
-    console.log ("rightHandler meghivodtam");
-    if (index < array.length-1) {
-        index++;
-        $(".activethumbnail").parent().next().children().first().trigger("click");
-    } else {
-        index = 0;
-        $(".thumbnails").children().first().children().first().trigger("click");
-    }
-    //refresh (index);
-}
-
-function hoverOn (event)  {
-    console.log ("hoverOn meghivodtam");
-    hovered = $(this).data('big');
-    $(".hint").text (array[hovered].hint);
-    let leftInitial = 451;
-    let thumbnailWidth = 75+4+4;
-    let left = leftInitial + hovered * (thumbnailWidth + 5);
-
-
-
-    //let positionString = "{top: -45px, left: " + left + "px, position:'absolute'}";
-    //$(".hint").css (positionString);
-    $(".hint").css ({top: -45 + "px", left: left + "px"});
-    //$(".hint").css("display", "block");
-    $(".hint").show();
-    //$("#mydiv").css({top: 200, left: 200, position:'absolute'});
-    console.log (left);
-    
-}
-
-function hoverOff (event)  {
-    console.log ("hoverOff meghivodtam");
-    $(".hint").hide();
-}
-
-
-$(".thumbnail").on ("click", imgHandler);
-$(".arrow.right").on ("click", rightHandler);
-$(".arrow.left").on ("click", leftHandler);
-$(".thumbnail").on ("mouseenter", hoverOn);
-$(".thumbnail").on ("mouseleave", hoverOff);
-
-$('.thumbnail.first').trigger ("click");
-
+  };
+  app.init();
+});
